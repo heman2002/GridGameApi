064124eb840537d22ab9747f3fa05ebaac982b76
himanshu singhal <himanshusinghal@inchus11.local>
2017-07-29 16:13:17 +0530

    Initial Commit

---
+++ GridGameApi/GridGameApi.h
@@ -0,0 +1,19 @@
+//
+//  GridGameApi.h
+//  GridGameApi
+//
+//  Created by himanshu singhal on 29/07/17.
+//  Copyright © 2017 himanshu singhal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GridGameApi.
+FOUNDATION_EXPORT double GridGameApiVersionNumber;
+
+//! Project version string for GridGameApi.
+FOUNDATION_EXPORT const unsigned char GridGameApiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GridGameApi/PublicHeader.h>
+
+
